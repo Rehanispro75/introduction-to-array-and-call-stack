305b13a53b0f06d7c88528e0699d785cb6241c2e
Rehanispro75 <rehanshaikh218053@gmail.com>
2024-09-14 19:49:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+
+
+var superhero = ["ironman" , "thor" , "batman"];
+superhero.pop();
+document.write(superhero[0]);
+
+
+
+
+
+document.write(superhero);
+
+
+
+var hero =superhero.pop()
+document.write(hero);
+
+
+function add(a,b){
+
+    return a+b;
+
+}
+
+function average(a,b){
+
+    return add(a,b)/2
+}
+
+let result = average (10,50)
+document.write(result)
+
+
+
